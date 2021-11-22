023c530782cd9de34e2e1295193850c950ca59a4
Ondrej Spanik <spanik11@msn.com>
2021-11-23 00:50:31 +0100

    better generator, hints, end

--- __main__.py
+++ __main__.py
@@ -6,31 +6,40 @@ from _evo import Evolution
 def main():
     colorama.init() # make termcolor work on windows
 
-    size = [12, 10]
-    maxGarden = 5
-    # size = [15, 15]
-    # size = [20, 5]
+    # sizes = [[12, 10], [15, 15], [20, 5]]
+    sizes = [[20, 5]]
+    maxGarden = 5 # files per samples folder 1 to 5
+    # evolutionCounts = [10, 100, 200, 500, 700, 1000]
+    evolutionCounts = [25]
+    evolutionRetries = 3
 
-    for i in range(maxGarden):
-        evolutionCount = 10
-        garden = GardenUtils.load("map_samples/" + str(size[0]) + "x" + str(size[1]) + "/" + str(i + 1))
-        garden.print()
-        evo = Evolution(garden = garden,
-                        populationCount = 50,
-                        fitnessStepOverRotWeight = 2,
-                        mutationChance = 0,
-                        crossPosition = 0,
-                        idealisticGenes = True,
-                        elitism = 0.1,
-                        gardenStepPrint = False,
-                        detailStepPrint = False,
-                        solutionPrint = False,
-                        elitismFitnessRecalcPrint = False)
+    for size in sizes:
+        for i in range(maxGarden):
+            gardenHolder = GardenUtils.load("map_samples/" + str(size[0]) + "x" + str(size[1]) + "/" + str(i + 1))
+            gardenHolder.print()
+            for r in range(evolutionRetries): # retry with fresh first generation
+                for evolutionCount in evolutionCounts: # try different counts of evolution
+                    garden = GardenUtils.copy(gardenHolder)
+                    evo = Evolution(garden = garden,
+                                    populationCount = 10,
+                                    fitnessStepOverRotWeight = 10,
+                                    mutationChance = 0.2,
+                                    crossPosition = garden.a,
+                                    idealisticGenes = True,
+                                    elitism = 0.7,
+                                    gardenStepPrint = False,
+                                    detailStepPrint = False,
+                                    solutionPrint = False,
+                                    elitismFitnessRecalcPrint = False)
 
-        for i in range(evolutionCount):
-            evo.evolve()
+                    print("[|", end = "")
+                    for j in range(evolutionCount):
+                        print("|", end = "")
+                        evo.evolve()
 
-        evo.successPrint(2) # change to 1 to print gardens of non-stuck monks that however still contain fields which haven't been visited
+                    print("]")
+
+                    evo.successPrint(1) # change to 1 to print gardens of non-stuck monks that however still contain fields which haven't been visited
 
     return
 
